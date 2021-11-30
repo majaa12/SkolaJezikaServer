86f700a95c9e15913e2b7bfb9ca3aad0adaf89ed
majaa12 <majacol98@gmail.com>
2021-11-30 14:57:58 +0100

    Dokumentovana sistemska operacija VratiGradoveSO koja je vezana za domensku klasu Grad.

--- src/main/java/rs/ac/bg/fon/nprog/server/operation/grad/VratiGradoveSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/server/operation/grad/VratiGradoveSO.java
@@ -5,10 +5,24 @@ import rs.ac.bg.fon.nprog.common.domain.Grad;
 import java.util.List;
 import rs.ac.bg.fon.nprog.server.operation.AbstractGenericOperation;
 
+/**
+ * Klasa koja predstavlja konkretnu sistemsku operaciju za vracanje svih gradova
+ * iz baze podataka.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
 public class VratiGradoveSO extends AbstractGenericOperation {
 
+	/**
+	 * Privatni atribut list tipa List{@literal <GenericEntity>} koji predstavlja
+	 * listu generickih objekata koje operacija treba da vrati.
+	 */
 	private List<GenericEntity> list;
 
+	/**
+	 * Proverava da li je prosledjeni parametar klase Grad.
+	 */
 	@Override
 	protected void preconditions(Object param) throws Exception {
 		if (!(param instanceof Grad)) {
@@ -16,11 +30,20 @@ public class VratiGradoveSO extends AbstractGenericOperation {
 		}
 	}
 
+	/**
+	 * Poziva brokera baze da pronadje listu svih gradova iz baze podataka.
+	 */
 	@Override
 	protected void executeOperation(Object param) throws Exception {
 		list = repository.getAll(param);
 	}
 
+	/**
+	 * Vraca privatni atribut list sa generickim objektima koje je operacija
+	 * vratila.
+	 * 
+	 * @return list kao List{@literal <GenericEntity>}
+	 */
 	public List<GenericEntity> getList() {
 		return list;
 	}
