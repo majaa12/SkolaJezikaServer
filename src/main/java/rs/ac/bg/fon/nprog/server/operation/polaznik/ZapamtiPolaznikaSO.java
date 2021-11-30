992e2b1fef22e6526e5de9428afe904988f68e58
majaa12 <majacol98@gmail.com>
2021-11-30 15:29:45 +0100

    Dokumentovane sistemske operacije ZapamtiPolaznikaSO i PretraziPolaznikeSO vezane za domensku klasu Polaznik.

--- src/main/java/rs/ac/bg/fon/nprog/server/operation/polaznik/ZapamtiPolaznikaSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/server/operation/polaznik/ZapamtiPolaznikaSO.java
@@ -3,8 +3,18 @@ package rs.ac.bg.fon.nprog.server.operation.polaznik;
 import rs.ac.bg.fon.nprog.common.domain.Polaznik;
 import rs.ac.bg.fon.nprog.server.operation.AbstractGenericOperation;
 
+/**
+ * Klasa koja predstavlja konkretnu sistemsku operaciju za dodavanje novog
+ * polaznika u bazu podataka.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
 public class ZapamtiPolaznikaSO extends AbstractGenericOperation {
 
+	/**
+	 * Proverava da li je prosledjeni parametar klase Polaznik.
+	 */
 	@Override
 	protected void preconditions(Object param) throws Exception {
 		if (!(param instanceof Polaznik)) {
@@ -12,6 +22,9 @@ public class ZapamtiPolaznikaSO extends AbstractGenericOperation {
 		}
 	}
 
+	/**
+	 * Poziva brokera baze da zapamti novog polaznika.
+	 */
 	@Override
 	protected void executeOperation(Object param) throws Exception {
 		repository.add(param);
