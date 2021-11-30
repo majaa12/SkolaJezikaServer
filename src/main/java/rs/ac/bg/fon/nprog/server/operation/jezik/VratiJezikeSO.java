ce3afe4e123f318ed019b59590a4641c6f8a6a10
majaa12 <majacol98@gmail.com>
2021-11-30 15:00:34 +0100

    Dokumentovana sistemska operacija VratiJezikeSO koja je vezana za domensku klasu Jezik.

--- src/main/java/rs/ac/bg/fon/nprog/server/operation/jezik/VratiJezikeSO.java
+++ src/main/java/rs/ac/bg/fon/nprog/server/operation/jezik/VratiJezikeSO.java
@@ -5,10 +5,24 @@ import rs.ac.bg.fon.nprog.common.domain.Jezik;
 import java.util.List;
 import rs.ac.bg.fon.nprog.server.operation.AbstractGenericOperation;
 
+/**
+ * Klasa koja predstavlja konkretnu sistemsku operaciju za vracanje svih jezika
+ * iz baze podataka.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
 public class VratiJezikeSO extends AbstractGenericOperation {
 
+	/**
+	 * Privatni atribut list tipa List{@literal <GenericEntity>} koji predstavlja
+	 * listu generickih objekata koje operacija treba da vrati.
+	 */
 	private List<GenericEntity> list;
 
+	/**
+	 * Proverava da li je prosledjeni parametar klase Jezik.
+	 */
 	@Override
 	protected void preconditions(Object param) throws Exception {
 		if (!(param instanceof Jezik)) {
@@ -16,11 +30,20 @@ public class VratiJezikeSO extends AbstractGenericOperation {
 		}
 	}
 
+	/**
+	 * Poziva brokera baze da pronadje listu svih jezika iz baze podataka.
+	 */
 	@Override
 	protected void executeOperation(Object param) throws Exception {
 		list = repository.getAll(param);
 	}
 
+	/**
+	 * Vraca privatni atribut list sa generickim objektima koje je operacija
+	 * vratila.
+	 * 
+	 * @return list kao List{@literal <GenericEntity>}
+	 */
 	public List<GenericEntity> getList() {
 		return list;
 	}
