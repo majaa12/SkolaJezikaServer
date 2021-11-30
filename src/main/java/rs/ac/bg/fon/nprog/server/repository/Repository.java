398b1657fa74539e56ed5832641267d5de6377dd
majaa12 <majacol98@gmail.com>
2021-11-30 16:13:44 +0100

    Dokumentovani interfejsi Repository i DbRepository koje implementira broker baze podataka.

--- src/main/java/rs/ac/bg/fon/nprog/server/repository/Repository.java
+++ src/main/java/rs/ac/bg/fon/nprog/server/repository/Repository.java
@@ -2,17 +2,65 @@ package rs.ac.bg.fon.nprog.server.repository;
 
 import java.util.List;
 
+/**
+ * Interfejs koji sadrzi genericke metode koje ce biti implementirane u brokeru
+ * baze podataka.
+ * 
+ * @author Maja
+ * @version 0.1
+ *
+ */
 public interface Repository<T> {
 
+	/**
+	 * Vraca sve objekte iz baze koji zadovoljavaju uslov pretrage.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @return lista objekata kao List{@literal <T>}.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
 	List<T> getAll(T param) throws Exception;
 
-    void add(T param) throws Exception;
+	/**
+	 * Dodaje prosledjeni parametar u bazu podataka.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
+	void add(T param) throws Exception;
 
-    void edit(T param) throws Exception;
+	/**
+	 * Vrsi izmene nad prosledjenim parametrom u bazi podataka.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
+	void edit(T param) throws Exception;
 
-    void delete(T param) throws Exception;
+	/**
+	 * Brise prosledjeni paramater iz baze podataka.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
+	void delete(T param) throws Exception;
 
-    T get(T param) throws Exception;
-    
-    T getWithJoin(T param) throws Exception;
+	/**
+	 * Vraca objekat iz baze koji odgovara prosledjenom parametru.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @return T koji je genericki tip objekta.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
+	T get(T param) throws Exception;
+
+	/**
+	 * Vraca objekat iz baze koji odgovara prosledjenom parametru, ali koristeci
+	 * join uslov u upitu.
+	 * 
+	 * @param param Parametar tipa T koji je genericki tip.
+	 * @return T koji je genericki tip objekta.
+	 * @throws Exception ukoliko dodje do greske prilikom izvrsavanja upita.
+	 */
+	T getWithJoin(T param) throws Exception;
 }
--- src/main/java/rs/ac/bg/fon/nprog/server/repository/db/DbRepository.java
+++ src/main/java/rs/ac/bg/fon/nprog/server/repository/db/DbRepository.java
@@ -2,20 +2,53 @@ package rs.ac.bg.fon.nprog.server.repository.db;
 
 import rs.ac.bg.fon.nprog.server.repository.Repository;
 
+/**
+ * Interfejs koji sadrzi default-ne metode kojima je odmah data implementacija.
+ * Nasledjuje Repository{@literal <T>} interfejs.
+ * 
+ * @author Maja
+ * @version 0.1
+ *
+ */
 public interface DbRepository<T> extends Repository<T> {
 
+	/**
+	 * Default-na metoda za uspostavljanje konekcije na bazu.
+	 * 
+	 * @throws Exception ukoliko dodje do greske prilikom otvaranje konekcije na
+	 *                   bazu
+	 */
 	default public void connect() throws Exception {
 		DbConnectionFactory.getInstance().getConnection();
 	}
 
+	/**
+	 * Default-na metoda za zatvaranje konekcije na bazu.
+	 * 
+	 * @throws Exception ukoliko dodje do greske prilikom zatvaranja konekcije na
+	 *                   bazu
+	 */
 	default public void disconnect() throws Exception {
 		DbConnectionFactory.getInstance().getConnection().close();
 	}
 
+	/**
+	 * Default-na metoda za potvrdu svih promena nastalih u transakciji, a od
+	 * prethodnog commit-a/rollback-a
+	 * 
+	 * @throws Exception ukoliko dodje do greske prilikom potvrdjivanja promena
+	 *                   nastalih tokom transakcije.
+	 */
 	default public void commit() throws Exception {
 		DbConnectionFactory.getInstance().getConnection().commit();
 	}
 
+	/**
+	 * Default-na metoda za ponistavanje efekata trenutne transakcije.
+	 * 
+	 * @throws Exception ukoliko dodje do greske prilikom ponistavanja efekata
+	 *                   transakcije.
+	 */
 	default public void rollback() throws Exception {
 		DbConnectionFactory.getInstance().getConnection().rollback();
 	}
