357e61354cd1d43ebd2f4388bfd995347c882841
majaa12 <majacol98@gmail.com>
2021-12-06 00:59:01 +0100

    Testirana klasa LoginSO.

--- src/test/java/rs/ac/bg/fon/nprog/server/operation/administrator/LoginSOTest.java
+++ src/test/java/rs/ac/bg/fon/nprog/server/operation/administrator/LoginSOTest.java
@@ -1,37 +1,29 @@
 package rs.ac.bg.fon.nprog.server.operation.administrator;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import rs.ac.bg.fon.nprog.common.domain.Administrator;
 import rs.ac.bg.fon.nprog.common.domain.GenericEntity;
 import rs.ac.bg.fon.nprog.common.domain.Jezik;
-import rs.ac.bg.fon.nprog.server.repository.Repository;
-import rs.ac.bg.fon.nprog.server.repository.db.impl.RepositoryDBGeneric;
 
 public class LoginSOTest {
 
 	private LoginSO operation;
-	private Repository repository;
 	private GenericEntity admin;
-
 
 	@BeforeEach
 	void setUp() throws Exception {
-		repository = mock(RepositoryDBGeneric.class);
-		operation = new LoginSO(repository);
-		admin = new Administrator(2l, "Jovan", "Maric", "jovan", "jovan");
+		operation = new LoginSO();
+		admin = new Administrator(1l, "Maja", "Colovic", "admin", "admin");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		operation = null;
-		repository = null;
 		admin = null;
 	}
 
@@ -46,13 +38,11 @@ public class LoginSOTest {
 		Administrator admin = new Administrator();
 		operation.preconditions(admin);
 	}
-	
+
 	@Test
-	void executeOperation_successful() throws Exception{
-		Mockito.doReturn(admin).when(repository).get(Mockito.any(Administrator.class));
-		
-		operation.executeOperation(admin);
-		assertEquals(admin, (Administrator)operation.getEntity());
+	void executeOperation_successful() throws Exception {
+		operation.execute(admin);
+		assertEquals(admin, (Administrator) operation.getEntity());
 	}
 
 }
